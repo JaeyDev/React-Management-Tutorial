15250f64053130dace7c71c1dffea2bed3447003
JaeyDev <spiritj1004@naver.com>
2021-10-10 03:58:06 +0900

    Add Progress

--- client/src/App.js
+++ client/src/App.js
@@ -9,19 +9,23 @@ import { TableRow } from '@mui/material';
 import { TableCell } from '@mui/material';
 import { withStyles } from '@material-ui/core/styles'; //신버전 안되서 구버전 설치함
 //import { makeStyles } from '@mui/material/styles';
+import CircularProgress from '@mui/material/CircularProgress'
 
 const styles = theme => ({
   root: {width: '100%', marginTop: theme.spacing.unit * 3, overflowX: "auto"},
-  table: {minWidth: 1080}
+  table: {minWidth: 1080},
+  process: { margin: theme.spacing.unit * 2 }
 })
 
 class App extends Component {
 
   state = {
-    customers: ""
+    customers: "",
+    completed: 0
   }
   
   componentDidMount(){
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
       .then(res => this.setState({customers: res}))
       .catch(err => console.log(err));
@@ -31,6 +35,11 @@ class App extends Component {
     const response = await fetch('/api/customers');
     const body = await response.json();
     return body;
+  }
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed : completed >= 200 ? 0 : completed + 1 });
   }
 
   render(){
@@ -51,7 +60,13 @@ class App extends Component {
             <TableBody>
               { this.state.customers ? this.state.customers.map(c => { 
                 return( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> ); 
-              }) : ""}
+              }) :
+              <TableRow>
+                <TableCell colSpan="6" align="center">
+                  <CircularProgress className={classes.process} variant="determinate" value={this.state.completed} />
+                </TableCell>
+              </TableRow>
+              }
             </TableBody>
           </Table>
         </Paper>
